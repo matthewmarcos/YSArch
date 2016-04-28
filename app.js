109511f68cf14cf52488fe1d78a75f764bfec3c4
Matthew Marcos <matthewmarcos94@gmail.com>
2016-04-28 18:31:19 +0800

    Add node_modules to .gitignore

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var app = express();
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const routes = require('./lib/router');
+const app = express();
 
 
 // view engine setup
